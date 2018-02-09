c1d9222b6ee864fd7bdc6a7ef19db234f9f3dde7
bob.lamsens <bob.lamsens@OntCur-0E18035F.vdabheverlee.intra>
2018-02-09 14:19:56 +0100

    Implement Vrachtwagen Class

--- AutoWorld/src/be/vdab/voertuigen/Vrachtwagen.java
+++ AutoWorld/src/be/vdab/voertuigen/Vrachtwagen.java
@@ -5,10 +5,55 @@
  */
 package be.vdab.voertuigen;
 
+import be.vdab.util.*;
+
 /**
  *
  * @author bob.lamsens
  */
-public class Vrachtwagen {
+public class Vrachtwagen extends Voertuig implements Laadbaar{
+
+    private Volume laadvolume;
+    private int maximaalToegelatenAssen;
+    private int aantalAssen;
+
+    public Vrachtwagen(Volume laadvolume, int maximaalToegelatenAssen, int aantalAssen, String merk, int aankoopprijs) {
+        super(merk, aankoopprijs);
+        this.laadvolume = laadvolume;
+        setMaximaalToegelatenAssen(maximaalToegelatenAssen);
+        setAantalAssen(aantalAssen);
+    }
+
+    public int getMaximaalToegelatenAssen() {
+        return maximaalToegelatenAssen;
+    }
+
+    public void setMaximaalToegelatenAssen(int maximaalToegelatenAssen) {
+        if (maximaalToegelatenAssen > 0)
+            this.maximaalToegelatenAssen = maximaalToegelatenAssen;
+        else
+            System.out.println("ongeldig max toegelaten aantal assen.");
+    }
+
+    public int getAantalAssen() {
+        return aantalAssen;
+    }
+
+    public void setAantalAssen(int aantalAssen) {
+        if (aantalAssen > 0)
+            this.aantalAssen = aantalAssen;
+        else
+            System.out.println("ongeldig aantal assen.");
+    }
     
+    @Override
+    public Volume getLaadVolume() {
+        return laadvolume;
+    }
+
+    @Override
+    public void setLaadVolume(Volume laadvolume) {
+        this.laadvolume = laadvolume;
+    }
+
 }
