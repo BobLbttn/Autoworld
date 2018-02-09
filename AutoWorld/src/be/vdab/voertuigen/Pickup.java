e84875a58e78a55477211156f5ee883fb6edbd44
bob.lamsens <bob.lamsens@OntCur-0E18035F.vdabheverlee.intra>
2018-02-09 14:03:49 +0100

    Implement Pickup Class

--- AutoWorld/src/be/vdab/voertuigen/Pickup.java
+++ AutoWorld/src/be/vdab/voertuigen/Pickup.java
@@ -5,10 +5,28 @@
  */
 package be.vdab.voertuigen;
 
+import be.vdab.util.*;
+
 /**
  *
  * @author bob.lamsens
  */
-public class Pickup {
+public class Pickup extends Personenwagen implements Laadbaar{
+    private Volume laadvolume;
+
+    public Pickup(Volume laadvolume, int zitplaatsen, String merk, int aankoopprijs) {
+        super(zitplaatsen, merk, aankoopprijs);
+        this.laadvolume = laadvolume;
+    }
+
+    @Override
+    public Volume getLaadVolume() {
+        return laadvolume;
+    }
+
+    @Override
+    public void setLaadVolume(Volume laadvolume) {
+        this.laadvolume = laadvolume;
+    }
     
 }
