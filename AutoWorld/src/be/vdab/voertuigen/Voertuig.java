ec590b5298450daaa6e2be838761a6db0ebff594
bob.lamsens <bob.lamsens@OntCur-0E18035F.vdabheverlee.intra>
2018-02-09 11:13:28 +0100

    Implementation class Voertuig

--- AutoWorld/src/be/vdab/voertuigen/Voertuig.java
+++ AutoWorld/src/be/vdab/voertuigen/Voertuig.java
@@ -5,10 +5,65 @@
  */
 package be.vdab.voertuigen;
 
+import be.vdab.voertuigen.div.*;
+import java.io.Serializable;
+
 /**
  *
  * @author bob.lamsens
  */
-public class Voertuig {
+public class Voertuig implements Comparable<Voertuig>, Serializable {
+    private final Nummerplaat nummerplaat = Div.INSTANCE.generateNummerplaat();
+    private String merk;
+    private int aankoopprijs;
+
+    public Voertuig(String merk, int aankoopprijs) {
+        setMerk(merk);
+        setAankoopprijs(aankoopprijs);
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        if (!merk.isEmpty())
+            this.merk = merk;
+        else
+            System.out.println("merk is niet ingevuld");
+    }
+
+    public int getAankoopprijs() {
+        return aankoopprijs;
+    }
+
+    public void setAankoopprijs(int aankoopprijs) {
+        if (aankoopprijs >= 0)
+            this.aankoopprijs = aankoopprijs;
+        else
+            System.out.println("aankoopprijs negatief");
+    }
     
+    @Override
+    public String toString(){
+        return nummerplaat.toString();
+    }
+    @Override
+    public int hashCode(){
+        int hash = 7;
+        return (hash*43)+nummerplaat.toString().hashCode();
+    }
+    @Override 
+    public boolean equals (Object o){
+        if (o instanceof Voertuig){
+            Voertuig v = (Voertuig) o;
+            return v.nummerplaat.equals(nummerplaat);
+        }
+        else
+            return false;
+    }
+    @Override
+    public int compareTo (Voertuig v){
+        return v.nummerplaat.compareTo(nummerplaat);
+    }
 }
