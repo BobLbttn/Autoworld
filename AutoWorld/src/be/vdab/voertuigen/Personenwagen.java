334abf619302a17b3cab7a9f4f7dc2da32b7355e
bob.lamsens <bob.lamsens@OntCur-0E18035F.vdabheverlee.intra>
2018-02-09 11:28:14 +0100

    Implement class Personenwagen

--- AutoWorld/src/be/vdab/voertuigen/Personenwagen.java
+++ AutoWorld/src/be/vdab/voertuigen/Personenwagen.java
@@ -9,6 +9,22 @@ package be.vdab.voertuigen;
  *
  * @author bob.lamsens
  */
-public class Personenwagen {
-    
+public class Personenwagen extends Voertuig {
+    private int zitplaatsen=1;
+
+    public Personenwagen(int zitplaatsen, String merk, int aankoopprijs) {
+        super(merk, aankoopprijs);
+        setZitplaatsen(zitplaatsen);
+    }
+
+    public int getZitplaatsen() {
+        return zitplaatsen;
+    }
+
+    public void setZitplaatsen(int zitplaatsen) {
+        if (zitplaatsen > 0)
+            this.zitplaatsen = zitplaatsen;
+        else
+            System.out.println("Een personenauto zonder zitplaatsen is absurd");
+    }
 }
